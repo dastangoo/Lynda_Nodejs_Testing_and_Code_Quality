640cd5fc6d9b731ce99919d9959da7a351615e74
dastangoo <hamed.dastangoo@gmail.com>
2017-10-31 13:31:47 +0330

    Linting your entire codebase

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     "es6": true
   },
   "extends": "eslint:recommended",
+  // "extends": "eslint-config-google",
   "rules": {
     "no-empty": "error",
     "no-multiple-empty-lines": "warn",
