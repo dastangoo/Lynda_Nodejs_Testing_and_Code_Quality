c0053be49f96c059021fa02264d38de8e505c093
dastangoo <hamed.dastangoo@gmail.com>
2017-10-31 13:58:31 +0330

    Cleaning code in Atom with ESLint

--- app.js
+++ app.js
@@ -1,30 +1,23 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-let index = require('./routes/index');
-let admin = require('./routes/admin');
-let reservations = require('./routes/reservations');
-
-
-const auth = require('./lib/middleware/auth.js');
-
-
-const configurations = {}
-
-
-
-
-
-
-
-if (1) {
-  
-}
+const express = require('express');
+// const path = require('path');
+// const favicon = require('favicon');
+// const logger = require('morgan');
+// const cookieParser = require('cookie-parser');
+// const bodyParser = require('body-parser');
+//
+// const index = require('./routes/index');
+// const admin = require('./routes/admin');
+// const reservations = require('./routes/reservations');
+//
+//
+// const auth = require('./lib/middleware/auth.js');
+//
+//
+// const configurations = {}
+//
+// if (1) {
+//
+// }
 
 
-var app = express();
-
+const app = express();
